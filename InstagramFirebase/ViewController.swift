9adf875429686804ce63eeb88b37a8f8d997712a
w3qiu <damon.q26@gmail.com>
2018-10-07 14:45:53 -0700

    Initial Commit

---
+++ InstagramFirebase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstagramFirebase
+//
+//  Created by wenlong qiu on 10/7/18.
+//  Copyright © 2018 wenlong qiu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
